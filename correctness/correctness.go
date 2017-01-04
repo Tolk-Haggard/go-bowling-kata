70e9a6de9077fc04874afed7dea251fcf0d31f65
Brian Haggard <brihagg@gmail.com>
2017-01-03 22:08:44 -0600

    Passing correctness test for complex game

--- correctness/correctness.go
+++ correctness/correctness.go
@@ -2,15 +2,38 @@ package correctness
 
 import "github.com/Tolk-Haggard/go-bowling-kata/scorer"
 
-type CorrectScorer struct{}
+type CorrectScorer struct {
+	firstRoll   bool
+	doublers    [21]int
+	currentRoll int
+	rolls       [21]int
+}
 
 func (c *CorrectScorer) RollBall(pins int) {
+	c.rolls[c.currentRoll] = pins
+	if c.firstRoll {
+		if pins == 10 {
+			c.doublers[c.currentRoll+1] = c.doublers[c.currentRoll+1] + 1
+			c.doublers[c.currentRoll+2] = c.doublers[c.currentRoll+2] + 1
+			c.firstRoll = false
+		}
+	} else {
+		if pins+c.rolls[c.currentRoll-1] == 10 {
+			c.doublers[c.currentRoll+1] = c.doublers[c.currentRoll+1] + 1
+		}
+	}
+
+	c.firstRoll = !c.firstRoll
+	c.currentRoll++
 }
 
 func (c *CorrectScorer) CalculateScore() (score int) {
-	return 0
+	for i := 0; i < 21; i++ {
+		score = score + c.rolls[i]*(c.doublers[i]+1)
+	}
+	return
 }
 
 func NewCorrectScorer() scorer.Scorer {
-	return &CorrectScorer{}
+	return &CorrectScorer{firstRoll: true}
 }
