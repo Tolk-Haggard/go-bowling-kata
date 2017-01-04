d87d66be629088b2b58e6541510dc69cae54a2c7
Brian Haggard <brihagg@gmail.com>
2017-01-03 23:03:38 -0600

    Refactor so method names describe what they do

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -3,15 +3,20 @@ package iteration
 import "github.com/Tolk-Haggard/go-bowling-kata/scorer"
 
 type IterativeScorer struct {
-	score int
+	rolls       [21]int
+	currentRoll int
 }
 
 func (i *IterativeScorer) RollBall(pins int) {
-	i.score += pins
+	i.rolls[i.currentRoll] = pins
+	i.currentRoll++
 }
 
-func (i *IterativeScorer) CalculateScore() int {
-	return i.score
+func (i *IterativeScorer) CalculateScore() (score int) {
+	for j := 0; j < 21; j++ {
+		score += i.rolls[j]
+	}
+	return
 }
 
 func NewIterativeScorer() scorer.Scorer {
