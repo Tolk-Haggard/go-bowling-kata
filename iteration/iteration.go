5e03e77df3fd0828ca68644a36a2ed8c4df2f61c
Brian Haggard <brihagg@gmail.com>
2017-01-03 22:50:12 -0600

    Failing first iterative test

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,1 +1,17 @@
 package iteration
+
+import "github.com/Tolk-Haggard/go-bowling-kata/scorer"
+
+type IterativeScorer struct {
+}
+
+func (I *IterativeScorer) RollBall(pins int) {
+}
+
+func (I *IterativeScorer) CalculateScore() int {
+	return -1
+}
+
+func NewIterativeScorer() scorer.Scorer {
+	return &IterativeScorer{}
+}
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,1 +1,32 @@
 package iteration_test
+
+import (
+	"testing"
+
+	"github.com/Tolk-Haggard/go-bowling-kata/iteration"
+	"github.com/Tolk-Haggard/go-bowling-kata/scorer"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ReturnsIterativeScoreGutterGame(t *testing.T) {
+	testObject := iteration.NewIterativeScorer()
+
+	rollGutterBalls(testObject, 20)
+
+	assert.Equal(t, 0, testObject.CalculateScore())
+}
+
+func rollStrike(s scorer.Scorer) {
+	s.RollBall(10)
+}
+
+func rollSpare(s scorer.Scorer) {
+	s.RollBall(5)
+	s.RollBall(5)
+}
+
+func rollGutterBalls(s scorer.Scorer, balls int) {
+	for i := 0; i < balls; i++ {
+		s.RollBall(0)
+	}
+}
