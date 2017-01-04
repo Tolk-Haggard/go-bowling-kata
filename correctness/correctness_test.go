776b12aba86a176a87d933c40b56a193a2fb6ac4
Brian Haggard <brihagg@gmail.com>
2017-01-03 22:08:34 -0600

    Failing correctness test for complex game

--- correctness/correctness_test.go
+++ correctness/correctness_test.go
@@ -16,3 +16,22 @@ func Test_ReturnsCorrectScoreGutterGame(t *testing.T) {
 
 	assert.Equal(t, 0, testObject.CalculateScore())
 }
+
+func Test_ReturnsCorrectScoreComplexGame(t *testing.T) {
+	testObject := correctness.NewCorrectScorer()
+
+	testObject.RollBall(10) //strike
+	testObject.RollBall(10) //strike
+	testObject.RollBall(5)  //spare 1
+	testObject.RollBall(5)  //spare 2
+	testObject.RollBall(1)
+	testObject.RollBall(1)
+	testObject.RollBall(9)
+	testObject.RollBall(0)
+
+	for i := 0; i < 13; i++ {
+		testObject.RollBall(0)
+	}
+
+	assert.Equal(t, 25+20+11+2+9, testObject.CalculateScore())
+}
