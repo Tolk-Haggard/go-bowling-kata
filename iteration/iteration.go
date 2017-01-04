233e2e59c349758a06565648dcc8ecd49f2905e7
Brian Haggard <brihagg@gmail.com>
2017-01-03 23:55:47 -0600

    Refactor iterative code

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -16,17 +16,24 @@ func (i *IterativeScorer) CalculateScore() (score int) {
 	roll := 0
 	for j := 0; j < 10; j++ {
 		if i.rolls[roll] == 10 {
-			score += 10 + i.rolls[j+1] + i.rolls[j+2]
-			roll++
+			score += i.handleStrike(roll)
+			roll--
 		} else if i.rolls[roll]+i.rolls[roll+1] == 10 {
-			score += 10 + i.rolls[roll+2]
-			roll += 2
+			score += i.handleSpare(roll)
 		} else {
 			score += i.rolls[roll] + i.rolls[roll+1]
-			roll += 2
 		}
+		roll += 2
 	}
 	return
+}
+
+func (i *IterativeScorer) handleStrike(roll int) int {
+	return 10 + i.rolls[roll+1] + i.rolls[roll+2]
+}
+
+func (i *IterativeScorer) handleSpare(roll int) int {
+	return 10 + i.rolls[roll+2]
 }
 
 func NewIterativeScorer() scorer.Scorer {
