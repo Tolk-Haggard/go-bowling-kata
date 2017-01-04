037e6bbd80f13e1b7cd2dd5f49d18bd01ae29b62
Brian Haggard <brihagg@gmail.com>
2017-01-03 22:07:06 -0600

    Pass first failing correctness test

--- correctness/correctness.go
+++ correctness/correctness.go
@@ -7,8 +7,8 @@ type CorrectScorer struct{}
 func (c *CorrectScorer) RollBall(pins int) {
 }
 
-func (c *CorrectScorer) CalculateScore() int {
-	return -1
+func (c *CorrectScorer) CalculateScore() (score int) {
+	return 0
 }
 
 func NewCorrectScorer() scorer.Scorer {
