0390423d56ed5ab1ac94b605fe16911a4553e518
Brian Haggard <brihagg@gmail.com>
2017-01-03 21:14:22 -0600

    First failing correctness test

--- correctness/correctness.go
+++ correctness/correctness.go
@@ -1,1 +1,16 @@
 package correctness
+
+import "github.com/Tolk-Haggard/go-bowling-kata/scorer"
+
+type CorrectScorer struct{}
+
+func (c *CorrectScorer) RollBall(pins int) {
+}
+
+func (c *CorrectScorer) CalculateScore() int {
+	return -1
+}
+
+func NewCorrectScorer() scorer.Scorer {
+	return &CorrectScorer{}
+}
--- correctness/correctness_test.go
+++ correctness/correctness_test.go
@@ -1,1 +1,18 @@
 package correctness_test
+
+import (
+	"testing"
+
+	"github.com/Tolk-Haggard/go-bowling-kata/correctness"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ReturnsCorrectScoreGutterGame(t *testing.T) {
+	testObject := correctness.NewCorrectScorer()
+
+	for i := 0; i < 20; i++ {
+		testObject.RollBall(0)
+	}
+
+	assert.Equal(t, 0, testObject.CalculateScore())
+}
