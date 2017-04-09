f7a31825996e866eb76c711ca3ffce8f63a426f8
Brian Haggard <brihagg@gmail.com>
2017-04-09 16:05:51 -0500

    Non tested listener example

---
+++ listener/nontested.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net"
+
+	"github.com/emersonsoftware/cg_listener/message"
+	"github.com/emersonsoftware/cg_listener/moderator"
+	"github.com/emersonsoftware/cg_listener/moderator/talker"
+	"github.com/emersonsoftware/cg_listener/waiter"
+)
+
+func main() {
+
+	waiter := waiter.NewSignalWaiter(make(chan bool))
+
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		// handle error
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+		close := make(chan bool, 5)
+		moderator := moderator.New(
+			talker.NewCTTalker(conn, message.NewClimateTalkMessageUtil(), close, waiter),
+			talker.NewMQTTTalker(message.NewMQTTMessageUtil(), mqttClientFactory, close, waiter),
+			talker.NewForkTalker(forkerClientFactory, close, waiter),
+			close,
+			waiter)
+		go handleConnection(moderator)
+	}
+
+}
